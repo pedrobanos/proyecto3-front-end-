f7349f2939bd3d5e87e514057b7a88932b1d1894
Miguel Baños <info.miguelbanos@gmail.com>
2022-09-20 23:36:14 +0200

    add style to Login view

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { login as loginRequest } from '../../services/AuthService';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import LogoGarage from '../../assets/logoGarage.png'
 import './Login.css'
 
 
@@ -57,15 +58,12 @@ const Login = () => {
     }
 
     return (
-        <div className="container Login">
-                <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="formContainer">
+                <form  className= "formLogin" onSubmit={handleSubmit(onSubmit)}>
                     <div className="row justify-content-center my-5">
                         <div className="col-sm-6 col-md-8 col-lg-8">
-                            <div className='text-center mb-4 mt-2'>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="120" height="120" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                                    <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                                </svg>
+                            <div className='logoContainer'>
+                               <img className='logoImg' src={LogoGarage}></img>
                             </div>
                             <div className='cifImputGroup'>
                                 <InputComponent className="input-group mb-4"
